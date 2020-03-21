7650e580f1a1e36f84c6e7d34efc01dea563557e
Sean Hinkle <seanhinkle46@gmail.com>
2020-03-21 17:28:18 -0400

    Problem 2 up and running. Both still need to be edited and commented

--- hinkle_problem2/src/Application.java
+++ hinkle_problem2/src/Application.java
@@ -1,6 +1,8 @@
 
 public class Application {
 	public static void main(String[] args) {
-		;
+		DuplicateCounter duplicateCounter = new DuplicateCounter();
+		duplicateCounter.count("problem2.txt");
+		duplicateCounter.write("unique_word_counts.txt");
 	}
 }
--- hinkle_problem2/src/DuplicateCounter.java
+++ hinkle_problem2/src/DuplicateCounter.java
@@ -1,13 +1,49 @@
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Set;
 
 public class DuplicateCounter {
 	//instance variables
+	Map<String, Integer> wordCounter = new HashMap<String, Integer>();
 	
 	public void count(String dataFile) {
-		
+		//some structure and map use inspired by figure 16.17 in textbook
+		try(Scanner input = new Scanner(Paths.get(dataFile))) {
+			while (input.hasNext()) {
+				String next = input.next().toLowerCase();
+				if (wordCounter.containsKey(next)) {
+					int current = wordCounter.get(next);
+					wordCounter.put(next, current + 1);
+				} else wordCounter.put(next, 1);
+			}
+		} catch (IOException e) {
+			System.err.print("There was an issue with the file opening");
+			e.printStackTrace();
+			System.exit(0);
+		}	
 	}
 	
 	public void write(String outputFile) {
-		
+		try (Formatter output = new Formatter(outputFile)) {
+			Set<String> words = wordCounter.keySet();
+			output.format("Key\t\tCount%n%n");
+			for (String s : words) {
+				output.format("%-10s%10d%n", s, wordCounter.get(s));
+			}
+		} catch (FileNotFoundException e) {
+			System.err.print("There was an issue opening/creating the file");
+			e.printStackTrace();
+			System.exit(0);
+		} catch (FormatterClosedException e) {
+			System.err.print("The formatter unexpectedly closed");
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 }
